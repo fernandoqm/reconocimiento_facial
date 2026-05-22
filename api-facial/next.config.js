8f9270f2e738aa5141e1e461e6287a7764d0b400
FernandoQMCR <fernando.quesadam@gmail.com>
2026-05-22 16:21:47 -0600

    fix: use correct Next.js 14 serverComponentsExternalPackages

--- api-facial/next.config.js
+++ api-facial/next.config.js
@@ -1,15 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  serverExternalPackages: [
-    '@tensorflow/tfjs-node',
-    'canvas',
-    '@vladmandic/face-api',
-    '@mapbox/node-pre-gyp',
-    'node-pre-gyp',
-  ],
+  experimental: {
+    // Next.js 14: excluir módulos nativos del bundle de webpack
+    serverComponentsExternalPackages: [
+      '@tensorflow/tfjs-node',
+      'canvas',
+      '@vladmandic/face-api',
+      '@mapbox/node-pre-gyp',
+      'node-pre-gyp',
+    ],
+  },
   webpack: (config, { isServer }) => {
     if (!isServer) return config;
-    // Evita que webpack trate de bundlear dependencias nativas de TF y canvas
     const nativeExternals = ['nock', 'aws-sdk', 'mock-aws-s3', '@mapbox/node-pre-gyp'];
     nativeExternals.forEach(pkg => config.externals.push(pkg));
     return config;
